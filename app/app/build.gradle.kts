d156117307bb01993bdb8a054eebf83523d9243c
0xAre <aryansyach4@gmail.com>
2026-08-04 17:16:40 +0700

    chore: bump version to 0.1.1-beta

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.example.caraka"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0-beta"
+        versionCode = 2
+        versionName = "0.1.1-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
